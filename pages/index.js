8724473d6c8d3d77325216770c26eda3b526697d
Muntasir <muntasirenamalhuda@gmail.com>
2021-06-01 13:10:33 +0100

    fixed form submission bug

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,14 @@ import Head from 'next/head';
 import { useState } from 'react';
 import homeStyle from '../styles/Home.module.scss';
 
+function encode(data) {
+	return Object.keys(data)
+		.map(
+			(key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]),
+		)
+		.join('&');
+}
+
 export default function home() {
 	const [info, setInfo] = useState({});
 
