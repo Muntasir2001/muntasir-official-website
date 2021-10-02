67aabec5d769c77a3826088005556008184b1c8c
Muntasir <muntasirenamalhuda@gmail.com>
2021-10-02 18:20:09 +0100

    made homepage (not navbar or footer)  half responsive

--- components/Utils.js
+++ components/Utils.js
@@ -3,7 +3,15 @@ import styled from 'styled-components';
 export const CommonLayout = styled.div`
 	margin: 0 140px;
 
+	@media screen and (max-width: 1544px) {
+		margin: 0 120px;
+	}
+
 	@media screen and (max-width: 1240px) {
 		margin: 0 100px;
+	}
+
+	@media screen and (max-width: 1024px) {
+		margin: 0 50px;
 	}
 `;
