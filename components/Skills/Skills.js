7037689f16496d6f37df5e08964a37b997c2b5ad
Muntasir <muntasirenamalhuda@gmail.com>
2022-05-08 17:22:42 +0100

    created skills component file

---
+++ components/Skills/Skills.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const SkillsParent = styled.div``;
+
+const Skills = () => {
+	return (
+		<>
+			<SkillsParent></SkillsParent>
+		</>
+	);
+};
+
+export default Skills;
