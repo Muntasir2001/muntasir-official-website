e17c101ee2f84f4814dd09f37842b58bc6fd160d
muntasir2001 <muntasirenamalhuda@gmail.com>
2022-05-24 17:26:56 +0100

    testing

--- public/skills-icon/htmlSVG.js
+++ public/skills-icon/htmlSVG.js
@@ -12,6 +12,7 @@ const HtmlIcon = ({ width, height, color }) => {
 			// xml:space='preserve'
 			// xmlns='http://www.w3.org/2000/svg'
 			// xmlns:xlink='http://www.w3.org/1999/xlink'
+			// heeho
 		>
 			<g>
 				<polygon
